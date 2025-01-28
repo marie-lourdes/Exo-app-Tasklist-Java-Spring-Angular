a86a1bfc10c6176af0b4cee107a2056f83de067e
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-01-28 17:07:46 +0100

    front-end/TaskService: add comments

--- frontend-app-tasklist-java-spring-angular/src/app/service/task.service.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/service/task.service.ts
@@ -3,6 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 //interface  cree pour l objet Task au lieu d une couche model avec une class Task
+/*Contrat de structure : Une interface définit un contrat que les objets doivent respecter. Cela garantit que les objets ont une structure cohérente,
+ce qui facilite la maintenance et la compréhension du code.
+Vérification de type : TypeScript utilise les interfaces pour effectuer la vérification de type à la compilation.
+Cela permet de détecter les erreurs potentielles avant l'exécution du code.  */
+
    export interface Task {
      id?: number;
      title: string;
@@ -11,9 +16,9 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class TaskService {
+export class  {
   //api vec springboot
   private apiUrl = 'http://localhost:8080/api/tasks';
-  // initialisation l'objet HttpClient
+  // initialisation de l'objet HttpClient
   constructor(private http: HttpClient) { }
 }
