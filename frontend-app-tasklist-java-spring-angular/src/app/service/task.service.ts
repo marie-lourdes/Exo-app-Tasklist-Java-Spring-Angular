fe74e595d7dde49b6880783e298233b21914af11
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-01-29 13:20:41 +0100

    front-end/TaskService: add methode HTTP

--- frontend-app-tasklist-java-spring-angular/src/app/service/task.service.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/service/task.service.ts
@@ -13,12 +13,36 @@ Cela permet de détecter les erreurs potentielles avant l'exécution du code.  *
      title: string;
      completed: boolean;
    }
+
+ // providedIn signifie a angular qu il sera charge a la racine de l application avec une seule et unique instance
+ /*Singleton : En déclarant le service avec providedIn: 'root', Angular s'assure qu'une seule instance du service est créée et partagée dans toute l'application.
+  Cela est utile pour des services qui gèrent des états partagés ou des ressources communes.
+   Tree-shakable : Avec providedIn: 'root', Angular peut éliminer le service du bundle final si aucune partie de l'application ne l'utilise.
+   Cela contribue à réduire la taille du bundle final.
+   Simplicité : En utilisant providedIn: 'root', tu n'as pas besoin de déclarer explicitement le service dans le tableau providers d'un module, ce qui simplifie la gestion des services.*/
 @Injectable({
   providedIn: 'root'
 })
-export class  {
+export class  TaskService{
   //api vec springboot
   private apiUrl = 'http://localhost:8080/api/tasks';
   // initialisation de l'objet HttpClient
   constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  createTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+
+  updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task);
+  }
+
+  deleteTask(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
 }
