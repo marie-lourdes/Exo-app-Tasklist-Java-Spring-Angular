3946638697814c1eba13a72352884a38fb456e1e
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-01-31 16:36:53 +0100

    front-end: remove file appmodule and kee provideHttpClient in app.config

--- frontend-app-tasklist-java-spring-angular/src/app/app.config.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
+//retrait du fichier  app module et l'import  HttpClientModule, BrowserModule  et ajout du provideHttpClient dans app config
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),provideHttpClient(withFetch())]
 };
