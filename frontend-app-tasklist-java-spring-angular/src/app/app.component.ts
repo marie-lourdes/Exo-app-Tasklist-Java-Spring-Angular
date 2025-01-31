7c88181261fc8b05afdd3c334fcf588109669c39
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-01-31 16:33:03 +0100

    fix compiler sass and import file

--- frontend-app-tasklist-java-spring-angular/src/app/app.component.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent} from  './home/home.component';
-import { TaskListComponent} from  './task-list/task-list.component';
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HomeComponent,TaskListComponent],
+  imports: [RouterOutlet,CommonModule,HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  iteration = new Array(5);
   title = 'frontend-app-tasklist-java-spring-angular';
   // lien configuré pour le dossier assets dans angular.json
   path ='./assets/images/logo.png';
--- frontend-app-tasklist-java-spring-angular/src/app/home/home.component.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/home/home.component.ts
@@ -1,15 +1,15 @@
 import { Component,OnInit } from '@angular/core';
 //import pour les directive ngfor et autres
 import {CommonModule} from '@angular/common';
+import { TaskListComponent} from  '../task-list/task-list.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [TaskListComponent,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
-  iteration = new Array(5);
 
   ngOnInit() : void{
     }
