a4416b190c4e6ef28e607846ce0bbe8a7afb04c4
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-02-03 14:50:29 +0100

    front-end: add withIncrementalHydration in app.config.ts and test @defer(on timer(15s); hydrate on interaction) in home.component.html

--- frontend-app-tasklist-java-spring-angular/src/app/app.config.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/app.config.ts
@@ -2,9 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient,withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay,withIncrementalHydration } from '@angular/platform-browser';
 
 //retrait du fichier  app module et l'import  HttpClientModule, BrowserModule  et ajout du provideHttpClient dans app config
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),provideHttpClient(withFetch())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay(),withIncrementalHydration()),provideHttpClient(withFetch())]
 };
