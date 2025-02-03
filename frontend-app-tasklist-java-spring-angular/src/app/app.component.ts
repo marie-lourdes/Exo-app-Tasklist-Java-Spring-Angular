429775513945cf2a74f5b5ffcca13c3d387aa161
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-02-03 15:48:35 +0100

    front-end: refactor app.component.html and structure of all components

--- frontend-app-tasklist-java-spring-angular/src/app/app.component.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TaskListComponent} from  './task-list/task-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- frontend-app-tasklist-java-spring-angular/src/app/home/home.component.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/home/home.component.ts
@@ -1,11 +1,8 @@
 import { Component,OnInit } from '@angular/core';
 //import pour les directive ngfor et autres
-import {CommonModule} from '@angular/common';
-import { TaskListComponent} from  '../task-list/task-list.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TaskListComponent,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
