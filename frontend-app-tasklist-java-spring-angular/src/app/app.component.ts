e21cad3087d1aad1a84d1471ce3abc78f95806c3
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-02-06 19:09:05 +0100

    front-end:  refactor structure of all component and refactor animation of banner in HomeComponent

--- frontend-app-tasklist-java-spring-angular/src/app/app.component.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoadSpinnerComponent} from  './load-spinner/load-spinner.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule, LoadSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
