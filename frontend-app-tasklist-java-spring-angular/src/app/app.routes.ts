3a4bf804500b4f6ab2339c7598ab9f16659ac346
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-02-07 13:29:38 +0100

    front-end: add AgendaComponent

--- frontend-app-tasklist-java-spring-angular/src/app/app.routes.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent} from  './home/home.component';
+import { AgendaComponent} from  './agenda/agenda.component';
 
 export const routes: Routes = [
-    {  path: '', component: HomeComponent}
+    {  path: '', component: HomeComponent},
+    {  path: 'agenda', component: AgendaComponent},
   ];
