d0e7a0b9276eb29f87198d9a66c1467ea5c4d26f
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-02-17 12:44:49 +0100

    front-end/ModalComponent: inject MAT-DIALOG-DATA and method onClose() for event close modal

--- frontend-app-tasklist-java-spring-angular/src/app/modal/modal.component.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/modal/modal.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-modal',
@@ -7,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './modal.component.css'
 })
 export class ModalComponent {
+  constructor(
+    public dialogRef: MatDialogRef <ModalComponent>,
+     @Inject(MAT_DIALOG_DATA) public data:{date:string}
+    }
+
+    onClose(): void {
+      this.dialogRef.close();
+    }
+
 
 }
