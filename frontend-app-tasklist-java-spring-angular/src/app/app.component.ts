043e2e00b39f2b858e06c42797c59d92b8d12684
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-02-27 17:07:36 +0100

    front-end: refactor AppComponent html

--- frontend-app-tasklist-java-spring-angular/src/app/app.component.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component,OnInit } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LoadSpinnerComponent} from  './load-spinner/load-spinner.component';
 import { HeaderComponent} from  './header/header.component';
 import { AgendaComponent} from  './agenda/agenda.component';
 
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule, HeaderComponent, LoadSpinnerComponent],
+  imports: [RouterOutlet,CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
