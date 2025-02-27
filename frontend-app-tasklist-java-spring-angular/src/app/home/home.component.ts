93cc7ee8b9d705483015949c1fe937311b1a7621
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-02-27 16:54:09 +0100

    front-end/HomeComponent html: add @defer with banner and TasklistComponent and remove it in AppComponent

--- frontend-app-tasklist-java-spring-angular/src/app/home/home.component.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component,OnInit } from '@angular/core';
 import { TaskListComponent} from  '../task-list/task-list.component';
+import { LoadSpinnerComponent} from  '../load-spinner/load-spinner.component';
 
 
 @Component({
   selector: 'app-home',
-  imports: [TaskListComponent],
+  imports: [TaskListComponent,LoadSpinnerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
