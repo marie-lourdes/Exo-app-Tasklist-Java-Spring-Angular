adfeb42fa635cf2e73399a86d25ee5cd43312dbe
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-03-01 16:26:30 +0100

    front-end/TaskService: refactor all methods CRUD with WritableSignal in TaskService

--- frontend-app-tasklist-java-spring-angular/src/app/service/task.service.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/service/task.service.ts
@@ -31,37 +31,79 @@ export class  TaskService implements OnInit {
   // Signal contenant toutes les tâches
   private tasks: WritableSignal<Task[]> = signal([]);
 
+
+ /* -L'utilisation de la méthode ngOnInit dans Angular est recommandée pour les opérations d'initialisation qui ne doivent être effectuées qu'une seule fois au cours du cycle de vie d'un composant.
+   Cela permet de s'assurer que les données requises pour le composant sont disponibles avant que celui-ci ne soit affiché à l'écran.
+  En faisant la requête dans la méthode ngOnInit, vous pouvez être sûr que les données sont chargées avant que le composant ne soit rendu, ce qui évite des erreurs potentielles lors de l'affichage de données manquantes ou non actualisées. Cela peut également aider à améliorer les performances en évitant d'exécuter des opérations inutiles ou coûteuses en temps d'exécution.
+     Charger toutes les tâches pour un usage global dans l'application ?
+     - Utilisez le `constructor`.
+     - C'est approprié lorsque les tâches doivent être disponibles dès le chargement initial de l'application ou lorsqu'elles sont partagées à travers plusieurs fonctionnalités dans l'application.
+
+  */
+
   // initialisation de l'objet HttpClient
   constructor(private http: HttpClient, @Inject(BASE_URL_API) private apiUrl: string) {
      console.log('Base URL:', this.apiUrl);
-
+     this.loadTasks(); // Charger les tâches initiales
   }
-
- /* L'utilisation de la méthode ngOnInit dans Angular est recommandée pour les opérations d'initialisation qui ne doivent être effectuées qu'une seule fois au cours du cycle de vie d'un composant.
-   Cela permet de s'assurer que les données requises pour le composant sont disponibles avant que celui-ci ne soit affiché à l'écran.
-  En faisant la requête dans la méthode ngOnInit, vous pouvez être sûr que les données sont chargées avant que le composant ne soit rendu, ce qui évite des erreurs potentielles lors de l'affichage de données manquantes ou non actualisées. Cela peut également aider à améliorer les performances en évitant d'exécuter des opérations inutiles ou coûteuses en temps d'exécution.*/
 
   ngOnInit () {
-   this.getTasks().subscribe();
+   //this.loadTasks();
   }
 
-  createTask(task: Task): Observable<Task> {
-    return this.http.post<Task>(this.apiUrl, task);
+  createTask(task: Task): void {
+     this.http.post<Task>(this.apiUrl, task).subscribe(
+      (newTask) =>{
+        this.tasks.update((tasks) => [...tasks, newTask]);
+      });
   }
 
-  updateTask(task: Task): Observable<Task> {
-    return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task);
+  updateTask(task: Task): void {
+    this.http.put<Task>(`${this.apiUrl}/${task.id}`, task).subscribe(
+       (updatedTask)=> {
+         /*- Attention il s agit de la méthode native **JavaScript Array.map()**.
+           - Elle itère sur chaque element du  tableau (les tâches, `Task[]`) pour les  transformer et retourner un nouveau tableau.
+           - Ce n'est pas l'opérateur `map`( avec pipe()) d'RxJS, qui aurait été utilisé avec des `Observable`.*/
+
+         this.tasks.update((tasks)=>
+           tasks.map((t)=> (t.id === updatedTask.id ? updatedTask : t ))
+         );
+       });
   }
 
-  deleteTask(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
-  }
-  getTasksByDate(date: string): Observable<Task[]> {
-    return this.http.get<Task[]>(`${this.apiUrl}/${date}`);
+  deleteTask(id: number): void {
+    this.http.delete<void>(`${this.apiUrl}/${id}`).subscribe(
+      ()=> {
+        this.tasks.update((tasks) => tasks.filter((t)=>
+          task.id !== id));
+    });
   }
 
-  getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.apiUrl);
+  getTasksByDate(date: string): WritableSignal<Task[]> {
+    // Créer un signal pour contenir les tâches par date
+    const tasksByDate: WritableSignal<Task[]> = signal([]);
+   // Effectuer la requête HTTP et mettre à jour le Signal
+   this.http.get<Task[]>(`${this.apiUrl}/${date}`).subscribe({
+     next: (tasks) => tasksByDate.set(tasks),
+     error: (err) => console.error('Error fetching tasks by date:', err)
+   });
+  // Retourner le Signal pour utiliser dans le composant Agenda
+   return tasksByDate;
+
+  }
+
+// Obtenez les tâches sous forme de signal
+  getTasks(): WritableSignal<Task[]> {
+    return this.tasks;
+  }
+
+
+// Charger à partir du backend et setter dans le signal
+  loadTasks(): void {
+    this.http.get<Task[]>(this.apiUrl).subscribe(
+      (tasks)=> {
+        this.tasks.set(tasks); // Mettre à jour le contenu du signal
+        });
   }
 
 }
