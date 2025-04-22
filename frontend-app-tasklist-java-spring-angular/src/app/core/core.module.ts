c01bdc31771ef685cc450ca2a3b6250dbf654a46
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-04-22 16:06:52 +0200

    front-end/refactoring-coremodule: import HeaderComponent, providers TaskService, ApiTaskService in CoreModule

--- frontend-app-tasklist-java-spring-angular/src/app/core/core.module.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/core/core.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from './components/header/header.component';
+import { ApiTaskService } from './services/api-task.service';
+import { TaskService } from './services/task.service';
+//ajout routermodule pour les directive router link dans le headercomponent
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent, // Composant global
+    ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent, // Exporté pour être réutilisé directement dans AppComponent
+  ],
+  providers: [
+    // Services globaux
+    TaskService,
+    ApiTaskService,
   ]
 })
 export class CoreModule { }
