c56ad355ce6559efaefd4f6d332a1a8e121ce8c0
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-04-22 18:07:00 +0200

    front-end/refactoring-architecture: add TODO in TaskListModule

--- frontend-app-tasklist-java-spring-angular/src/app/features/task-list/tasklist.module.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/features/task-list/tasklist.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TasklistRoutingModule } from './tasklist-routing.module';
-
+//TODO: Ajouter core module qui dans son provieder a deja TaskService et terminer les routes pour chaque composants
 
 @NgModule({
   declarations: [],
