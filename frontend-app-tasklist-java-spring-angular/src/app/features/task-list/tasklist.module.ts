f0c854cd14b3562a2336eeb9f7f832b6d3af7004
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-04-24 16:18:19 +0200

    front-end/refactoring architecture: create TaskListModule, iport SharedModule, CoreModule, TaslistComponent as standalone component

--- frontend-app-tasklist-java-spring-angular/src/app/features/task-list/tasklist.module.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/features/task-list/tasklist.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { TasklistRoutingModule } from './tasklist-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { CoreModule } from '../core/core.module';
+import { TaskListComponent } from './task-list.component'; // ajuste le chemin selon ta structure
+//import { TasklistRoutingModule } from './tasklist-routing.module';
 //TODO: Ajouter core module qui dans son provieder a deja TaskService et terminer les routes pour chaque composants
 
+//Les composants stanlone sont exportable automatiquement, pas besoin d exporter TaskListComponent
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    TasklistRoutingModule
+    SharedModule,
+    CoreModule,
+    TaskListComponent
   ]
 })
 export class TasklistModule { }
