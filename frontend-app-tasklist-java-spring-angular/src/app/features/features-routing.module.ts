0efb27e2a8acc71f28b1dce318d97733a5ce9114
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-04-24 17:00:11 +0200

    front-end/refactoring-routing:  load path of AgendaModule, HomeModule, TaslistModule in FeaturesRoutingModul as child pathe

--- frontend-app-tasklist-java-spring-angular/src/app/features/features-routing.module.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/features/features-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const featuresRoutes: Routes = [
+   {
+      path: '',
+      loadChildren: () => import('./home/home.module')
+        .then(m => m.HomeModule)
+    },
+    {
+      path: 'agenda',
+      loadChildren: () => import('./agenda/agenda.module')
+        .then(m => m.AgendaModule)
+    },
+    {
+      path: 'tasks',
+      loadChildren: () => import('./task-list/task-list.module')
+        .then(m => m.TaskListModule)
+    }
+
+  ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(featuresRoutes)],
   exports: [RouterModule]
 })
 export class FeaturesRoutingModule { }
