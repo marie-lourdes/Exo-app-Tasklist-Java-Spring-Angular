65b8482552c4001afa623aa42d383676ed1384cd
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-05-05 17:39:14 +0200

    front-end/refactoring-architecture: create config directory in /core with app.config , app.confid.server

--- frontend-app-tasklist-java-spring-angular/src/app/core/index.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/core/index.ts
@@ -1,9 +1,16 @@
 // core/index.ts regroupe les service utlisés par les composants et facilite l import
+
+// Services
 export * from './http/services/api-task.service';
 export * from './services/task.service';
+
+// Components
 export * from './components/header/header.component';
+
+// Interfaces
 export * from './http/interfaces/icrudoperation.interface';
 
+// Providers
 import { ApiTaskService } from './http/services/api-task.service';
 import { TaskService } from './services/task.service';
 import { InjectionToken } from '@angular/core';
