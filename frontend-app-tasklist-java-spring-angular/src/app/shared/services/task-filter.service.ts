4a4425d624addb6938929496b104674f3cd63d53
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-05-20 16:44:01 +0200

    front-end/refactoring-architecture-PatternStrategy: create method filterByStatus() in  TaskFilterService

---
+++ frontend-app-tasklist-java-spring-angular/src/app/shared/services/task-filter.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+/* service qui centralise les filtrages des tâches
+*/
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskFilterService {
+
+  constructor(
+    private readonly completedTasksStrategy: CompletedTasksStrategy,
+    private readonly pendingTasksStrategy: PendingTasksStrategy,
+    private readonly dateFilteringStrategy: DateFilteringStrategy
+    ) {}
+
+  filterByStatus(tasks: ITask[], status: TaskStatus): ITask[] {
+    return status === TaskStatus.COMPLETED
+    ? this.completedTasksStrategy.sort(tasks)
+    : this.pendingTasksStrategy.sort(tasks);
+    }
+
+}
