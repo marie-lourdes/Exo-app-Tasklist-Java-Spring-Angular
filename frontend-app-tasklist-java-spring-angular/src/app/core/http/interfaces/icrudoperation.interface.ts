6e2f76a7c34f35bb23410ac3a95704d642270c6c
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-05-20 17:32:53 +0200

    front-end/refactoring-architecture-PatternFacade: add TODO for refactoring with PatterFacade and Segregation Interface

--- frontend-app-tasklist-java-spring-angular/src/app/core/http/interfaces/icrudoperation.interface.ts
+++ frontend-app-tasklist-java-spring-angular/src/app/core/http/interfaces/icrudoperation.interface.ts
@@ -1,6 +1,9 @@
 import { Observable } from 'rxjs';
 import { ITask } from '@app/shared';
 
+//TODO: Creer deux interfaces reader and writer qu heritera IcrudOperation
+//et applique le PatterFacade et Segregation des Interfaces avec taskface qui cree selon le role indiqué une classe ou service
+//qui implement IReader ou IWriter
 export interface ICrudOperation{
    createTask(task: ITask): Observable<ITask>;
    updateTask(task: ITask, onComplete: () => void): Observable<ITask>;
